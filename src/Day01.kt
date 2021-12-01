23a387b6efaaaddb1e50f71e85704f62492d772a
Ashish Sadhwani <2ashishs@gmail.com>
2021-12-02 02:02:16 +0530

    Day-1

--- src/Day01.kt
+++ src/Day01.kt
@@ -8,10 +8,22 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+//    println(part1(input))
+//    println(part2(input))
+
+    var count = 0
+    var curr: Int
+    var prev: Int = input[0].toInt()
+    for (idx in input.indices) {
+        curr = input[idx].toInt()
+        if (idx > 0 && curr > prev) {
+            count++
+        }
+        prev = curr
+    }
+    println(count)
 }
