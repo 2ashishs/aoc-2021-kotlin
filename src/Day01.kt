045cee7116f840732f8d319108f677dcee394991
Ashish Sadhwani <2ashishs@gmail.com>
2021-12-02 13:40:03 +0530

    Day-1 (Part-2)

--- src/Day01.kt
+++ src/Day01.kt
@@ -14,12 +14,22 @@ fun main() {
     val input = readInput("Day01")
 //    println(part1(input))
 //    println(part2(input))
-
+    //Part-1
     var count = 0
     var curr: Int
     var prev: Int = input[0].toInt()
     for (idx in input.indices) {
         curr = input[idx].toInt()
+        if (idx > 0 && curr > prev) {
+            count++
+        }
+        prev = curr
+    }
+    println(count)
+    //Part-2 3-measurement sliding window
+    count = 0
+    for (idx in 0..input.size - 3) {
+        curr = input[idx].toInt() + input[idx + 1].toInt() + input[idx + 2].toInt()
         if (idx > 0 && curr > prev) {
             count++
         }
